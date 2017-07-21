4164daaa310bf6b885705404480563d513a15379
iyaja <ajayuppili@gmail.com>
2017-07-22 04:40:54 +0530

    Added gradle dependancies and a method for connecting to watson and fetching results.

--- app/src/main/java/com/example/admin/visualgseg/MainActivity.java
+++ app/src/main/java/com/example/admin/visualgseg/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.admin.visualgseg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
+
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int a;
+
+    VisualClassification pushToWatson (String filePath){//filePath would store image location (eg:-"src/test/resources/visual_recognition/car.png")
+        VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+        service.setApiKey("145b047be11f5059687578f4ca85325d23e0cdf8");
+
+        ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
+                .images(new File("src/test/resources/visual_recognition/car.png"))//passes filePath to analyze
+                .build();
+        VisualClassification result = service.classify(options).execute();
+        return result;
+    }
 }
