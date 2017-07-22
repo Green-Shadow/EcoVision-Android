97130feba6f9080e1d848e0d8e1fb2929d90e40e
kamtechie <kamtechie@gmail.com>
2017-07-22 13:24:32 +0000

    Added camra methods + required permissions

--- app/src/main/java/com/example/admin/visualgseg/MainActivity.java
+++ app/src/main/java/com/example/admin/visualgseg/MainActivity.java
@@ -17,14 +17,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    VisualClassification pushToWatson (String filePath){                                            //filePath would store image location (eg:-"src/test/resources/visual_recognition/car.png")
+    VisualClassification pushToWatson (File photoFile){                                            //filePath would store image location (eg:-"src/test/resources/visual_recognition/car.png")
         VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
         service.setApiKey("145b047be11f5059687578f4ca85325d23e0cdf8");
 
         ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
-                .images(new File(filePath))                                                         //passes filePath to analyze
+                .images(photoFile)                                                         //passes filePath to analyze
                 .build();
         VisualClassification result = service.classify(options).execute();
         return result;
     }
+    private File createImageFile() throws IOException {
+    // Create an image file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String imageFileName = "JPEG_" + timeStamp + "_";
+    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    File image = File.createTempFile(
+        imageFileName,  /* prefix */
+        ".jpg",         /* suffix */
+        storageDir      /* directory */
+    );
+
+    // Save a file: path for use with ACTION_VIEW intents
+    //mCurrentPhotoPath = image.getAbsolutePath();
+    return image;
+}
+private File takePhoto() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        File photoFile = null;
+        try {photoFile = createImageFile();} 
+        catch (IOException ex) {
+           ex.printStackTrace();
+        }
+        if (photoFile != null) {
+            Uri photoURI = Uri.fromFile(photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, 1);
+            return photoFile;
+        }
+    }
+    return null;
+}
 }
