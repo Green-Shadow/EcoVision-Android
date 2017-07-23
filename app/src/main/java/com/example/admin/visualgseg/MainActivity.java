1f3c598927a3a94d54291ab5334422b551b2823c
kamtechie <kamtechie@gmail.com>
2017-07-23 19:36:32 +0530

    Make button launch pushToWatson method for testing SDK + Efficiency changes

--- app/src/main/java/com/example/admin/visualgseg/MainActivity.java
+++ app/src/main/java/com/example/admin/visualgseg/MainActivity.java
@@ -27,44 +27,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    VisualClassification pushToWatson (File photoFile){                                            //filePath would store image location (eg:-"src/test/resources/visual_recognition/car.png")
+    public String pushToWatson (File photoFile, View view){                                           
         VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
         service.setApiKey("145b047be11f5059687578f4ca85325d23e0cdf8");
 
         ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
-                .images(photoFile)                                                         //passes filePath to analyze
+                .images(takePhoto())                                                         //calls takePhoto() to launch camera app
                 .build();
         VisualClassification result = service.classify(options).execute();
-        return result;
+        return result.toString();
     }
-    private File createImageFile() throws IOException {
-    // Create an image file name
+    private File takePhoto() throws IOException{ //Consolidated both image handling methods for code hygeine
     String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
     String imageFileName = "JPEG_" + timeStamp + "_";
-    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-    File image = File.createTempFile(
-        imageFileName,  /* prefix */
-        ".jpg",         /* suffix */
-        storageDir      /* directory */
-    );
-
-    // Save a file: path for use with ACTION_VIEW intents
-    //mCurrentPhotoPath = image.getAbsolutePath();
-    return image;
-}
-    public File takePhoto(View view) {
+    File image = File.createTempFile(imageFileName,".jpg",getExternalFilesDir(Environment.DIRECTORY_PICTURES));
     Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-        File photoFile = null;
-        try {photoFile = createImageFile();} 
-        catch (IOException ex) {
-           ex.printStackTrace();
-        }
         if (photoFile != null) {
-            Uri photoURI = Uri.fromFile(photoFile);
+            Uri photoURI = Uri.fromFile(image);
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
             startActivityForResult(takePictureIntent, 1);
-            return photoFile;
+            return image;
         }
     }
     return null;
